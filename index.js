52510c7d67a4ebc65c2d552e4ac6abba4e7b69a8
Apple <apple@Akash-Apple-pros.local>
2023-04-18 16:21:05 +0530

    issue fixed

--- index.js
+++ index.js
@@ -12,60 +12,68 @@ app.listen(8000 || process.env.PORT, () => {
     console.log('Webhook is listening');
 })
 
-app.get('/', (req, res) => {res.status(200).json({'message': 'OK'})});
+app.get('/', (req, res) => { res.status(200).json({ 'message': 'OK' }) });
 
 app.get('/webhook', async (req, res) => {
-    console.log("🚀 ~ file: index.js:19 ~ app.get ~ req:", req)
+    try {
+        console.log("🚀 ~ file: index.js:19 ~ app.get ~ req:", req)
 
-    const mode = req.query['hub.mode'];
-    console.log("🚀 ~ file: index.js:21 ~ app.get ~ mode:", mode)
-    const challenge = req.query['hub.challenge'];
-    console.log("🚀 ~ file: index.js:23 ~ app.get ~ challenge:", challenge)
-    const token = req.query['hub.verify_token'];
-    console.log("🚀 ~ file: index.js:25 ~ app.get ~ token:", token)
+        const mode = req.query['hub.mode'];
+        console.log("🚀 ~ file: index.js:21 ~ app.get ~ mode:", mode)
+        const challenge = req.query['hub.challenge'];
+        console.log("🚀 ~ file: index.js:23 ~ app.get ~ challenge:", challenge)
+        const token = req.query['hub.verify_token'];
+        console.log("🚀 ~ file: index.js:25 ~ app.get ~ token:", token)
 
-    if (mode && token) {
-        console.log("🚀 ~ file: index.js:28 ~ app.get ~ mode && token:", mode && token)
-        if (mode === 'subscribe' && token === myToken) {
-            res.status(200).send(challenge)
+        if (mode && token) {
+            console.log("🚀 ~ file: index.js:28 ~ app.get ~ mode && token:", mode && token)
+            if (mode === 'subscribe' && token === myToken) {
+                res.status(200).send(challenge)
+            }
+            else {
+                res.status(403)
+            }
         }
-        else {
-            res.status(403)
-        }
+    } catch (error) {
+        return res.status(500).json({ error })
     }
 });
 
 app.post('/webhook', async (req, res) => {
-    const body = req.body;
-    console.log(JSON.stringify(body, null, 2))
+    try {
+        const body = req.body;
+        console.log(JSON.stringify(body, null, 2))
 
-    if (body.object) {
-        if (body.entry &&
-            body.entry[0].changes &&
-            body.entry[0].changes[0].value.message &&
-            body.entry[0].changes[0].value.message[0]) {
+        if (body.object) {
+            if (body.entry &&
+                body.entry[0].changes &&
+                body.entry[0].changes[0].value.message &&
+                body.entry[0].changes[0].value.message[0]) {
 
-            const phoneNoId = body.entry[0].changes[0].value.metadata.phone_number_id;
-            const from = body.entry[0].changes[0].value.message[0].from;
-            const msgBody = body.entry[0].changes[0].value.message[0].text.body;
+                const phoneNoId = body.entry[0].changes[0].value.metadata.phone_number_id;
+                const from = body.entry[0].changes[0].value.message[0].from;
+                const msgBody = body.entry[0].changes[0].value.message[0].text.body;
 
-            const url = 'https://graph.facebook.com/v16.0' + phoneNoId + '/message?access_token=' + token;
-            const payload = {
-                messaging_product: 'whatsapp',
-                to: from,
-                text: {
-                    body: 'Hello World Boi'
-                }
-            };
-            const headers = {
-                'Content-Type': 'application/json'
-            };
-            const apiCall = await doPostRequest(url, payload, headers);
-            res.status(200).send(apiCall)
+                const url = 'https://graph.facebook.com/v16.0' + phoneNoId + '/message?access_token=' + token;
+                const payload = {
+                    messaging_product: 'whatsapp',
+                    to: from,
+                    text: {
+                        body: 'Hello World Boi'
+                    }
+                };
+                const headers = {
+                    'Content-Type': 'application/json'
+                };
+                const apiCall = await doPostRequest(url, payload, headers);
+                res.status(200).send(apiCall)
+            }
+            else {
+                res.status(403)
+            }
         }
-        else {
-            res.status(403)
-        }
+    } catch (error) {
+        return res.status(500).json({ error })
     }
 });
 
