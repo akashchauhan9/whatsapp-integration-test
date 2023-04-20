ff274a7a226de4ad236c7b012be907b7ca90c18f
Apple <apple@Akash-Apple-pros.local>
2023-04-20 18:23:34 +0530

    form submit condition change with en and hi

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ app.post('/webhook', async (req, res) => {
                             name: body.entry[0].changes[0].value.contacts[0].profile.name,
                             mobile: from,
                             step: 1,
-                            lang: msgBody
+                            lang: Number(msgBody)
                         };
                         user.splice(index, 1);
                         user.push(userExist);
